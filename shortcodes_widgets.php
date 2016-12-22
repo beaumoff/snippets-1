7468774793b5dcc8d04ed321b32d1482fdac341d
Karena Kreger <karenalenore@gmail.com>
2016-12-22 08:48:14 -0500

    Update shortcodes_widgets.php

--- shortcodes_widgets.php
+++ shortcodes_widgets.php
@@ -1,5 +1,7 @@
+<?php
 // Execute Shortcodes in Widgets and Excerpts
 add_filter( 'widget_text', 'shortcode_unautop' );
 add_filter( 'widget_text', 'do_shortcode' );	
 add_filter( 'the_excerpt', 'shortcode_unautop' );
 add_filter( 'the_excerpt', 'do_shortcode' );
+?>
