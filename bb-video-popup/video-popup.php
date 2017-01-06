f369c2b36f7859a2cb83052cf32b52907e540a01
Karena Kreger <karenalenore@gmail.com>
2017-01-06 11:50:00 -0500

    Create video-popup.php

---
+++ bb-video-popup/video-popup.php
@@ -0,0 +1,9 @@
+<?php
+//* Use a Beaver Builder image to link to a Video Popup.
+add_action( 'wp_enqueue_scripts', 'opensky_video_popup_thumbnail' );
+function opensky_video_popup_thumbnail() {
+	wp_enqueue_script( 'jquery_magnificpopup_min_js', plugins_url() . '/bb-plugin/js/jquery.magnificpopup.min.js', array('jquery'), '', true );
+	wp_enqueue_style( 'jquery_magnificpopup', plugins_url( ) .'/bb-plugin/css/jquery.magnificpopup.css' );
+	wp_enqueue_script( 'jquery_custom_js', get_stylesheet_directory_uri().'/js/jquery-custom.js', array('jquery'), '', true );
+}
+?>
