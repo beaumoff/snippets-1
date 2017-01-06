2a735bd758cfd0f3b0759c09aa158d16cb5c9e81
Karena Kreger <karenalenore@gmail.com>
2017-01-06 11:54:37 -0500

    Update video-popup.php

--- bb-video-popup/video-popup.php
+++ bb-video-popup/video-popup.php
@@ -4,6 +4,6 @@ add_action( 'wp_enqueue_scripts', 'opensky_video_popup_thumbnail' );
 function opensky_video_popup_thumbnail() {
 	wp_enqueue_script( 'jquery_magnificpopup_min_js', plugins_url() . '/bb-plugin/js/jquery.magnificpopup.min.js', array('jquery'), '', true );
 	wp_enqueue_style( 'jquery_magnificpopup', plugins_url( ) .'/bb-plugin/css/jquery.magnificpopup.css' );
-	wp_enqueue_script( 'jquery_custom_js', get_stylesheet_directory_uri().'/js/jquery-custom.js', array('jquery'), '', true );
+	wp_enqueue_script( 'jquery_video_popup_js', get_stylesheet_directory_uri().'/js/video-popup.js', array('jquery'), '', true );
 }
 ?>
