44b0e52079610337ce79c15c557aa11a3f4bd4ab
Karena Kreger <karenalenore@gmail.com>
2017-10-12 15:39:26 -0400

    Added Support for Onpage Link Offset

--- smoothscroll_to_anchor.js
+++ smoothscroll_to_anchor.js
@@ -1,11 +1,13 @@
 // Smooth Scroll to Anchor Link Location, Adjust for Sticky Header height and Responsive Breakpoint
 jQuery( document ).ready(function($) {
 
+    var headerHeight = 127;
+    var mobileBreakpoint = 960;
+
     if ( window.location.hash ) scroll(0,0); // void some browsers issue
     setTimeout( function() { scroll(0,0); }, 1);
 
     $(function() {
-
         // *only* if we have anchor on the url and if it exists on the page.
         if($(window.location.hash).length) { 
 
@@ -13,9 +15,9 @@ jQuery( document ).ready(function($) {
             var headerOffset = 0;
             var scrollPos = hashPos - headerOffset;
             // change position to offset header height if not on mobile
-            if ($(window).width() > 960) { // mobile breakpoint
+            if ($(window).width() > mobileBreakpoint) {
                 hashPos = $(window.location.hash).offset().top;
-                headerOffset = 100; // header height
+                headerOffset = headerHeight;
                 scrollPos = hashPos - headerOffset;
             }
             // smooth scroll to the anchor id
@@ -23,6 +25,11 @@ jQuery( document ).ready(function($) {
                 scrollTop: scrollPos + 'px'
             }, 1000, 'swing');
         }
-
+        if ( 'undefined' != typeof FLBuilderLayoutConfig ) {
+            if ($(window).width() > mobileBreakpoint) {
+                FLBuilderLayoutConfig.anchorLinkAnimations.offset = headerHeight;
+            }
+        }
     });
 });
+
