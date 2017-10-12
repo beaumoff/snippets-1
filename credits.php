498ede595cd6ef5a6dddfbb6697f5b3238966bdc
Karena Kreger <karenalenore@gmail.com>
2017-10-12 13:29:37 -0400

    Added Linebreak variable, fixed Site Design text

--- credits.php
+++ credits.php
@@ -1,22 +1,26 @@
 <?php
 // Copyright Credit Shortcode
-// Usage: [credits oscredit="false" title="" link="" link_title=""]
+// Usage: [credits oscredit="false" title="" link="" link_title="" linebreak="true"]
+add_filter('genesis_footer_creds_text', 'opensky_credits_function');
 add_shortcode('credits', 'opensky_credits_function');
 function opensky_credits_function( $atts, $content = null ) {
    extract( shortcode_atts( array(
       'oscredit' => 'true', //set true|false for link to Open Sky
       'title' => 'Open Sky Web Studio',
       'link' => 'http://www.openskywebstudio.com',
-      'link_title' => 'Open Sky Web Studio | Clean, effective Websites',
+      'link_title' => 'Open Sky Web Studio | Clean, Effective Websites',
+      'linebreak' => 'true',
       ), $atts ) );
-
-	$creds = "Copyright &copy; " . date('Y') . " <a href='".get_option('home')."' title='".get_bloginfo('name')." | ".get_bloginfo('description')."'>".get_bloginfo('name')."</a>. All Rights Reserved. ";
+	$creds = "<div id='credits'>Copyright &copy; " . date('Y') . " <a href='".get_option('home')."' title='".get_bloginfo('name')." | ".get_bloginfo('description')."'>".get_bloginfo('name')."</a>. All Rights Reserved. ";
 	if (strtolower($oscredit) == "false")
 		$creds .= "<!--";
-	$creds .= "<a href='".$link."' title='".$link_title."' target='_blank'>".$title."</a> &middot; ";
+	if (strtolower($linebreak) == "true")
+		$creds .= "<br/>";
+	$creds .= "Site Design: <a href='".$link."' title='".$link_title."' target='_blank'>".$title."</a> &middot; ";
 	if (strtolower($oscredit) == "false")
 		$creds .= "-->";
-	$creds .= wp_loginout('', false) . wp_register(' &middot; ', '', false) ;
+	$creds .= wp_loginout('', false) . wp_register(' &middot; ', '', false) . '</div>';
 	return $creds;
 }
+
 ?>
